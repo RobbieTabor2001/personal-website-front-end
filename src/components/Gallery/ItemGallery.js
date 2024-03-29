330bcfb92501415528d79483dc0144f329749fbf
RobbieTabor2001 <robert.tabor2001@gmail.com>
2024-03-29 09:56:10 -0400

    add web

--- src/components/Gallery/ItemGallery.js
+++ src/components/Gallery/ItemGallery.js
@@ -1,78 +1,86 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { Masonry, useInfiniteLoader } from 'masonic';
-import MasonryCard from './MasonryCard';// Ensure this path is correct
+import MasonryCard from './MasonryCard'; // Ensure this path is correct
 
 const ItemGallery = () => {
   const [items, setItems] = useState([]);
-  const [columnWidth, setColumnWidth] = useState(300);
-  const [columnGutter, setColumnGutter] = useState(20);
+  const [cursors, setCursors] = useState({ single: null, multi: null });
+  const [hasMoreItems, setHasMoreItems] = useState(true);
+  const limit = 25; // Set the number of items to fetch per request
 
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+    }
+    return array;
+  };
+
+  const preloadImage = (src) => new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = src;
+    img.onload = resolve;
+    img.onerror = reject;
+  });
+
+  // Function to fetch items, adapted for pagination
+  const fetchItems = useCallback(async (cursorSingle, cursorMulti, limit) => {
+    const apiUrl = `${process.env.REACT_APP_API_BASE_URL}/api/paginated-images?limit=${limit}` +
+                   `${cursorSingle ? `&cursorSingle=${cursorSingle}` : ''}` +
+                   `${cursorMulti ? `&cursorMulti=${cursorMulti}` : ''}`;
+    const response = await fetch(apiUrl);
+    if (!response.ok) throw new Error('Failed to fetch items');
+    const data = await response.json();
+
+    // Process and return data
+    return {
+      nextCursorMulti: data.nextCursorMulti,
+      nextCursorSingle: data.nextCursorSingle,
+      multiImages: data.multImages,
+      singleImages: data.singleImages, // Combine single and multi-image item
+      images: data.images
+    };
+  }, []);
+
+  // Load more items function, adapted for cursor-based pagination
   const loadMoreItems = useCallback(async (startIndex, stopIndex) => {
-    // Here, you'd typically make an API call to fetch more items.
-    // Since you're not using pagination, you can duplicate existing items or cycle through them.
-    const newItems = [...items]; // Duplicate the current items for the example
-    setItems((currentItems) => [...currentItems, ...newItems]);
-  }, [items]);
+    if (!hasMoreItems) return;
+    try {
+      const {images, nextCursorSingle, nextCursorMulti } = await fetchItems(cursors.single, cursors.multi, limit);
+      
+              // Adapt single image items
+              let adaptedItems = images.map(item => ({
+                ...item,
+                compressedURL: item.medium.compressed,
+                losslessURL: item.medium.lossless,
+              }));
 
+              adaptedItems = shuffleArray(adaptedItems);
+                      // Preload small compressed images for faster UI rendering
+        await Promise.all(adaptedItems.map(item => preloadImage(item.compressedURL)));
+      setItems((currentItems) => [...currentItems, ...adaptedItems]);
+      setCursors({ single: nextCursorSingle, multi: nextCursorMulti });
+      // Update hasMoreItems based on whether cursors are present
+      console.log(nextCursorMulti);
+      console.log(nextCursorSingle);
+      console.log("fcuirha");
+      setHasMoreItems((nextCursorSingle !== null && nextCursorMulti !== null));
+      console.log((nextCursorSingle !== null && nextCursorMulti !== null));
+    } catch (error) {
+      console.error('Error loading more items:', error);
+    }
+  }, [cursors, hasMoreItems, fetchItems, limit]);
 
   const maybeLoadMore = useInfiniteLoader(loadMoreItems, {
     isItemLoaded: (index, items) => !!items[index],
-    minimumBatchSize: 50, // Number of items to load at once
-    threshold: 3, // How far from the end to start loading more
+    minimumBatchSize: limit,
+    threshold: 3,
   });
 
-
   useEffect(() => {
-    const shuffleArray = (array) => {
-      for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]]; // Swap elements
-      }
-      return array;
-    };
-
-    const preloadImage = (src) => new Promise((resolve, reject) => {
-      const img = new Image();
-      img.src = src;
-      img.onload = resolve;
-      img.onerror = reject;
-    });
-
-    const fetchItems = async () => {
-      try {
-        const apiUrl = `${process.env.REACT_APP_API_BASE_URL}/api/images`;
-        const response = await fetch(apiUrl);
-        const { allSingleImageURLs, allMultiImageUrls } = await response.json();
-    
-        // Adapt single image items
-        let adaptedSingleImagesItems = allSingleImageURLs.map(item => ({
-          ...item,
-          compressedURL: item.sizes.medium.compressed,
-          losslessURL: item.sizes.medium.lossless,
-        }));
-    
-        // Adapt multi image items
-        let adaptedMultiImages = allMultiImageUrls.map(item => ({
-          ...item,
-          compressedURL: item.medium.compressed,
-          losslessURL: item.medium.lossless,
-        }));
-    
-        // Merge adapted single and multi-image items
-        let adaptedItems = adaptedSingleImagesItems.concat(adaptedMultiImages);
-        adaptedItems = shuffleArray(adaptedItems);
-    
-        // Preload small compressed images for faster UI rendering
-        await Promise.all(adaptedItems.map(item => preloadImage(item.compressedURL)));
-    
-        setItems(adaptedItems);
-      } catch (error) {
-        console.error('Failed to fetch items:', error);
-      }
-    };
-    
-    fetchItems();
-  }, []);
+    // Initial fetch is just a special case of loading more items
+    loadMoreItems(0, limit);
+  }, [loadMoreItems, limit]);
 
   const scrollToTop = () => {
     window.scrollTo({
